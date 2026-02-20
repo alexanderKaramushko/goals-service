4a8759e43891cc3d973516d1e81c6d77186f9619
a.karamushko <a.karamushko@tbank.ru>
2026-02-20 21:00:30 +0400

    доработка сообщений авторизации

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -2,7 +2,6 @@ import {
   CanActivate,
   ExecutionContext,
   Injectable,
-  NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
 import { AuthMicroserviceService } from 'src/modules/microservices/auth/auth-microservice.service';
@@ -14,24 +13,20 @@ export class AuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
 
-    try {
-      if (!request.cookies.jwt) {
-        throw new NotFoundException(`JWT-токен не найден`);
-      }
+    if (!request.cookies.jwt) {
+      throw new UnauthorizedException();
+    }
 
-      const [user] = await this.authMicroserviceService.getUserByJwt(
-        request.cookies.jwt as string,
-      );
+    const [user] = await this.authMicroserviceService.getUserByJwt(
+      request.cookies.jwt as string,
+    );
 
-      if (user) {
-        request.user = user;
+    if (user) {
+      request.user = user;
 
-        return !!user;
-      } else {
-        throw new UnauthorizedException('Пользователь не найден');
-      }
-    } catch (error: unknown) {
-      throw new NotFoundException(`Пользователь не найден: ${error as string}`);
+      return !!user;
+    } else {
+      throw new UnauthorizedException('Пользователь не найден');
     }
   }
 }
