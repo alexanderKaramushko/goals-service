7fa97d1194e62c43cbf2923d6864ffef36989e29
a.karamushko <a.karamushko@tbank.ru>
2026-07-09 21:23:13 +0400

    CI & deploy

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-version=$1
+tag=$1
 
-docker build --platform linux/amd64 -t melkor73/goals-service:$version -t melkor73/goals-service:latest $PWD
+docker build --platform linux/amd64 -t melkor73/goals-service:$tag -t melkor73/goals-service:latest $PWD
 
-docker push melkor73/goals-service:$version
+docker push melkor73/goals-service:$tag
 docker push melkor73/goals-service:latest
